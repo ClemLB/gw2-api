5fbcd98d551173fcecf4671cfdf0ba65da01c550
Clément Le Borgne <clement.leborgne@atos.net>
2022-10-15 19:06:12 +0200

    Commit initial

---
+++ src/test/java/fr/kuremento/gw2/Gw2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kuremento.gw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gw2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
