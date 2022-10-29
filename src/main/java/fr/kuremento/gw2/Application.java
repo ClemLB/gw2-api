3837414b244abfc9ffd2a05f99b63781526c3e50
Clément Le Borgne <clement.leborgne@atos.net>
2022-10-29 21:47:54 +0200

    Refactoring

--- src/main/java/fr/kuremento/gw2/Application.java
+++ src/main/java/fr/kuremento/gw2/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.List;
-
 @Slf4j
 @SpringBootApplication
 public class Application {
@@ -20,8 +18,7 @@ public class Application {
 	}
 
 	private static void execute(Gw2Client gw2Client, String apiKey) {
-		var list = gw2Client.achievements().groups().getAll();
-		list.forEach(achievementGroup -> log.info("{}", achievementGroup));
+
 	}
 
 }
