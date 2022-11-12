4cc58c302d853c2eace0a9469ed52d76611fefde
Clément Le Borgne <clement.leborgne@atos.net>
2022-11-12 16:00:42 +0100

    Ajout du service des miniatures

--- src/main/java/fr/kuremento/gw2/web/rest/services/minis/MinisService.java
+++ src/main/java/fr/kuremento/gw2/web/rest/services/minis/MinisService.java
@@ -1,9 +1,14 @@
 package fr.kuremento.gw2.web.rest.services.minis;
 
+import fr.kuremento.gw2.exceptions.TooManyArgumentsException;
+import fr.kuremento.gw2.web.rest.models.minis.Miniature;
 import fr.kuremento.gw2.web.rest.services.AbstractService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +17,23 @@ public class MinisService extends AbstractService {
 	@Value("${application.rest.endpoints.minis}")
 	private final String endpoint;
 
+	public List<Integer> get() {
+		return super.get(super.buildURI(endpoint), new ParameterizedTypeReference<>() {
+		});
+	}
+
+	public List<Miniature> getAll() {
+		return super.get(super.buildURIAllIds(endpoint), new ParameterizedTypeReference<>() {
+		});
+	}
+
+	public List<Miniature> get(List<Integer> ids) throws TooManyArgumentsException {
+		return super.get(super.buildURI(endpoint, ids), new ParameterizedTypeReference<>() {
+		});
+	}
+
+	public Miniature get(Integer id) {
+		return super.get(super.buildURI(endpoint, id), new ParameterizedTypeReference<>() {
+		});
+	}
 }
