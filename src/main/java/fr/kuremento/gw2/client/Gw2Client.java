3229e72d9912a76066e2301c65da330665586162
Clément Le Borgne <clement.leborgne@atos.net>
2022-11-12 16:11:55 +0100

    Code smell SonarLint

--- src/main/java/fr/kuremento/gw2/client/Gw2Client.java
+++ src/main/java/fr/kuremento/gw2/client/Gw2Client.java
@@ -7,6 +7,7 @@ import fr.kuremento.gw2.web.rest.services.colors.ColorsService;
 import fr.kuremento.gw2.web.rest.services.quaggans.QuaggansService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import reactor.core.publisher.Mono;
 
 @Component("gw2-client")
@@ -18,22 +19,22 @@ public class Gw2Client {
 	private final QuaggansService quaggansService;
 	private final ColorsService colorsService;
 
-	public static Mono<Throwable> getErrorConsumerForError401() {
-		return Mono.error(new TechnicalException(
-				"The requested endpoint is authenticated and you did not provide a valid API key, or a valid API key without the necessary permissions"));
+	public static Mono<Throwable> getErrorConsumerForError401(ClientResponse response) {
+		return Mono.error(new TechnicalException(response.statusCode() +
+		                                         " : The requested endpoint is authenticated and you did not provide a valid API key, or a valid API key without the necessary permissions"));
 	}
 
-	public static Mono<Throwable> getErrorConsumerForError403() {
-		return Mono.error(new TechnicalException(
-				"The requested endpoint is authenticated and you did not provide a valid API key, or a valid API key without the necessary permissions"));
+	public static Mono<Throwable> getErrorConsumerForError403(ClientResponse response) {
+		return Mono.error(new TechnicalException(response.statusCode() +
+		                                         " : The requested endpoint is authenticated and you did not provide a valid API key, or a valid API key without the necessary permissions"));
 	}
 
-	public static Mono<Throwable> getErrorConsumerForError404() {
-		return Mono.error(new TechnicalException("The requested endpoint does not exist, or all of the provided IDs are invalid"));
+	public static Mono<Throwable> getErrorConsumerForError404(ClientResponse response) {
+		return Mono.error(new TechnicalException(response.statusCode() + " : The requested endpoint does not exist, or all of the provided IDs are invalid"));
 	}
 
-	public static Mono<Throwable> getErrorConsumerForError503() {
-		return Mono.error(new TechnicalException("The requested endpoint is disabled"));
+	public static Mono<Throwable> getErrorConsumerForError503(ClientResponse response) {
+		return Mono.error(new TechnicalException(response.statusCode() + " : The requested endpoint is disabled"));
 	}
 
 	public AccountService account() {
