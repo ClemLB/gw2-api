034683b8c969b019aec2d7eaece12c9b53a0f804
Clément Le Borgne <clement.leborgne@atos.net>
2022-12-01 14:56:44 +0100

    Ajout de TU
    Signed-off-by: Clément Le Borgne <clement.leborgne@atos.net>

---
+++ src/test/java/fr/kuremento/gw2/web/rest/services/traits/TraitsTest.java
@@ -0,0 +1,61 @@
+package fr.kuremento.gw2.web.rest.services.traits;
+
+import fr.kuremento.gw2.exceptions.TooManyArgumentsException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TraitsTest {
+
+	@Autowired
+	private TraitsService service;
+
+	@Value("${application.rest.config.page-maximum-size}")
+	private Integer maxPageSize;
+
+	@Test
+	@DisplayName("Check number of raids")
+	void test1() {
+		assertFalse(service.get().isEmpty(), "Service should return a list of raids id");
+	}
+
+	@Test
+	@DisplayName("Check max number of raids per request exception is thrown")
+	void test2() {
+		var fakeIdsList = Arrays.stream(IntStream.generate(() -> new Random().nextInt(10000)).limit(maxPageSize + 1).toArray()).boxed().toList();
+		Exception exception = assertThrows(TooManyArgumentsException.class, () -> service.get(fakeIdsList));
+
+		String actualMessage = exception.getMessage();
+		assertTrue(actualMessage.contains("Maximum number of arguments"));
+	}
+
+	@Test
+	@DisplayName("Check max number of raids per request")
+	void test3() {
+		var fakeIdsList = List.of(265);
+		var list = assertDoesNotThrow(() -> service.get(fakeIdsList));
+		assertTrue(list.size() <= maxPageSize, String.format("Service should return at most %d raids", maxPageSize));
+	}
+
+	@Test
+	@DisplayName("Check request one raid")
+	void test4() {
+		assertNotNull(service.get(265), "Requested raid should not be null");
+	}
+
+	@Test
+	@DisplayName("Check getAll request")
+	void test5() {
+		assertFalse(service.getAll().isEmpty(), "Service should return a list of raids");
+	}
+}
