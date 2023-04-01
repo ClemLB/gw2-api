d8a6eca62eb7e80ab433d151cd8d93bc99507ea5
Clément Le Borgne <clement.leborgne@atos.net>
2023-04-01 18:23:29 +0200

    Ajout de la recherche de l'armurerie légendaire
    Signed-off-by: Clément Le Borgne <clement.leborgne@atos.net>
    #minor

---
+++ src/main/java/fr/kuremento/gw2/web/rest/models/account/legendaryarmory/LegendaryItem.java
@@ -0,0 +1,17 @@
+package fr.kuremento.gw2.web.rest.models.account.legendaryarmory;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LegendaryItem {
+
+    @JsonProperty("id")
+    String id;
+
+    @JsonProperty("count")
+    Integer count;
+}
