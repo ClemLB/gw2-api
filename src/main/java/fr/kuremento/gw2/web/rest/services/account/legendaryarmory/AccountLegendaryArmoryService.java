72d2ab940593865e1eda0e16615aed09eee8d987
Kurement0 <clem.leborgne@gmail.com>
2023-04-01 18:33:34 +0200

    155 accountlegendaryarmory (#156)
    ## Describe your changes
    
    ## Issue ticket number and link
    
    ## Checklist before requesting a review
    - [ ] I have performed a self-review of my code
    - [ ] If it is a core feature, I have added thorough tests.
    
    Type of bump : #minor
    
    ---------
    
    Signed-off-by: Clément Le Borgne <clement.leborgne@atos.net>
    Co-authored-by: Clément Le Borgne <clement.leborgne@atos.net>

---
+++ src/main/java/fr/kuremento/gw2/web/rest/services/account/legendaryarmory/AccountLegendaryArmoryService.java
@@ -0,0 +1,26 @@
+package fr.kuremento.gw2.web.rest.services.account.legendaryarmory;
+
+import fr.kuremento.gw2.web.rest.models.account.legendaryarmory.LegendaryItem;
+import fr.kuremento.gw2.web.rest.services.AbstractService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://wiki.guildwars2.com/wiki/API:2/account/legendaryarmory">Wiki</a>
+ */
+@Service
+@RequiredArgsConstructor
+public class AccountLegendaryArmoryService extends AbstractService {
+
+	@Value("${application.rest.endpoints.account-category.legendary-armory}")
+	private final String endpoint;
+
+	public List<LegendaryItem> getWithAuthentification(String apiKey) {
+		return super.getWithAuthentification(super.buildURI(endpoint), new ParameterizedTypeReference<>() {
+		}, apiKey);
+	}
+}
