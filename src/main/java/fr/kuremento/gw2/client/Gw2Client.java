bea437739a1370e8457dfdf894161b99952a8c29
Clément Le Borgne <clement.leborgne@atos.net>
2023-04-08 14:20:01 +0200

    Ajout du service sur les tokens
    Signed-off-by: Clément Le Borgne <clement.leborgne@atos.net>

--- src/main/java/fr/kuremento/gw2/client/Gw2Client.java
+++ src/main/java/fr/kuremento/gw2/client/Gw2Client.java
@@ -11,6 +11,7 @@ import fr.kuremento.gw2.web.rest.services.minis.MinisService;
 import fr.kuremento.gw2.web.rest.services.quaggans.QuaggansService;
 import fr.kuremento.gw2.web.rest.services.raids.RaidsService;
 import fr.kuremento.gw2.web.rest.services.traits.TraitsService;
+import fr.kuremento.gw2.web.rest.tokeninfo.TokenInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ClientResponse;
@@ -20,66 +21,71 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class Gw2Client {
 
-	private final AccountService accountService;
-	private final AchievementsService achievementsService;
-	private final QuaggansService quaggansService;
-	private final ColorsService colorsService;
-	private final HomeService homeService;
-	private final MinisService minisService;
-	private final RaidsService raidsService;
-	private final ItemsService itemsService;
-	private final TraitsService traitsService;
+    private final AccountService accountService;
+    private final AchievementsService achievementsService;
+    private final QuaggansService quaggansService;
+    private final ColorsService colorsService;
+    private final HomeService homeService;
+    private final MinisService minisService;
+    private final RaidsService raidsService;
+    private final ItemsService itemsService;
+    private final TraitsService traitsService;
+    private final TokenInfoService tokenInfoService;
 
-	public static Mono<Throwable> getErrorConsumerForError401(ClientResponse response) {
-		return Mono.error(new TechnicalException(response.statusCode() + " : " + Constants.ERROR_401_403_MESSAGE.getValue()));
-	}
+    public static Mono<Throwable> getErrorConsumerForError401(ClientResponse response) {
+        return Mono.error(new TechnicalException(response.statusCode() + " : " + Constants.ERROR_401_403_MESSAGE.getValue()));
+    }
 
-	public static Mono<Throwable> getErrorConsumerForError403(ClientResponse response) {
-		return Mono.error(new TechnicalException(response.statusCode() + " : " + Constants.ERROR_401_403_MESSAGE.getValue()));
-	}
+    public static Mono<Throwable> getErrorConsumerForError403(ClientResponse response) {
+        return Mono.error(new TechnicalException(response.statusCode() + " : " + Constants.ERROR_401_403_MESSAGE.getValue()));
+    }
 
-	public static Mono<Throwable> getErrorConsumerForError404(ClientResponse response) {
-		return Mono.error(new TechnicalException(response.statusCode() + " : " + Constants.ERROR_404_MESSAGE.getValue()));
-	}
+    public static Mono<Throwable> getErrorConsumerForError404(ClientResponse response) {
+        return Mono.error(new TechnicalException(response.statusCode() + " : " + Constants.ERROR_404_MESSAGE.getValue()));
+    }
 
-	public static Mono<Throwable> getErrorConsumerForError503(ClientResponse response) {
-		return Mono.error(new TechnicalException(response.statusCode() + " : " + Constants.ERROR_503_MESSAGE.getValue()));
-	}
+    public static Mono<Throwable> getErrorConsumerForError503(ClientResponse response) {
+        return Mono.error(new TechnicalException(response.statusCode() + " : " + Constants.ERROR_503_MESSAGE.getValue()));
+    }
 
-	public AccountService account() {
-		return this.accountService;
-	}
+    public AccountService account() {
+        return this.accountService;
+    }
 
-	public AchievementsService achievements() {
-		return this.achievementsService;
-	}
+    public AchievementsService achievements() {
+        return this.achievementsService;
+    }
 
-	public QuaggansService quaggans() {
-		return this.quaggansService;
-	}
+    public QuaggansService quaggans() {
+        return this.quaggansService;
+    }
 
-	public ColorsService colors() {
-		return this.colorsService;
-	}
+    public ColorsService colors() {
+        return this.colorsService;
+    }
 
-	public HomeService home() {
-		return this.homeService;
-	}
+    public HomeService home() {
+        return this.homeService;
+    }
 
-	public MinisService minis() {
-		return this.minisService;
-	}
+    public MinisService minis() {
+        return this.minisService;
+    }
 
-	public RaidsService raids() {
-		return this.raidsService;
-	}
+    public RaidsService raids() {
+        return this.raidsService;
+    }
 
-	public ItemsService items() {
-		return this.itemsService;
-	}
+    public ItemsService items() {
+        return this.itemsService;
+    }
 
-	public TraitsService traits() {
-		return this.traitsService;
-	}
+    public TraitsService traits() {
+        return this.traitsService;
+    }
+
+    public TokenInfoService tokeninfo() {
+        return this.tokenInfoService;
+    }
 
 }
---
+++ src/test/java/fr/kuremento/gw2/web/rest/services/tokeninfo/TokenInfoTest.java
@@ -0,0 +1,44 @@
+package fr.kuremento.gw2.web.rest.services.tokeninfo;
+
+import fr.kuremento.gw2.exceptions.TechnicalException;
+import fr.kuremento.gw2.models.Constants;
+import fr.kuremento.gw2.web.rest.models.tokeninfo.TokenInfo;
+import fr.kuremento.gw2.web.rest.tokeninfo.TokenInfoService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TokenInfoTest {
+
+    @Autowired
+    private TokenInfoService service;
+
+    @Value("${test.api-key.account}")
+    private String apiKey;
+
+    @Test
+    @DisplayName("Check no authentification exception")
+    void test1() {
+        Exception exception = assertThrows(TechnicalException.class, () -> {
+            service.getWithAuthentification("");
+        });
+
+        String actualMessage = exception.getMessage();
+        assertEquals("401 UNAUTHORIZED" + " : " + Constants.ERROR_401_403_MESSAGE.getValue(), actualMessage);
+    }
+
+    @Test
+    @DisplayName("Check authentification ok")
+    void test2() {
+        AtomicReference<TokenInfo> tokenInfo = new AtomicReference<>();
+        assertDoesNotThrow(() -> tokenInfo.set(service.getWithAuthentification(apiKey)));
+        assertNotNull(tokenInfo.get(), "Service should return informations on token");
+    }
+}
