ed640b7c478e0ce07f807b0abbb49dc4ce73c166
Kurement0 <clem.leborgne@gmail.com>
2023-05-05 17:40:19 +0200

    Création du service AccountHomeService (#162)
    ## Describe your changes
    
    ## Issue ticket number and link
    #21 
    
    Type of bump : #minor
    
    Co-authored-by: Clément <clement.leborgne@reseau.eseo.fr>

---
+++ src/main/java/fr/kuremento/gw2/web/rest/services/account/home/AccountHomeService.java
@@ -0,0 +1,10 @@
+package fr.kuremento.gw2.web.rest.services.account.home;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountHomeService {
+
+}
