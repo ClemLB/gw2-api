b29fc7a58dd615b8ca636d416b43a72b2e84fb1b
Kurement0 <clem.leborgne@gmail.com>
2023-05-11 21:00:04 +0200

    Ajout du service des chats (#165)
    ## Describe your changes
    
    ## Issue ticket number and link
    #94 
    
    Type of bump : #minor

---
+++ src/main/java/fr/kuremento/gw2/web/rest/models/home/cats/Cat.java
@@ -0,0 +1,17 @@
+package fr.kuremento.gw2.web.rest.models.home.cats;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Cat {
+
+    @JsonProperty("id")
+    Integer id;
+
+    @JsonProperty("hint")
+    String hint;
+}
--- src/main/java/fr/kuremento/gw2/web/rest/services/home/HomeService.java
+++ src/main/java/fr/kuremento/gw2/web/rest/services/home/HomeService.java
@@ -1,5 +1,6 @@
 package fr.kuremento.gw2.web.rest.services.home;
 
+import fr.kuremento.gw2.web.rest.services.home.cats.HomeCatsService;
 import fr.kuremento.gw2.web.rest.services.home.nodes.HomeNodesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,14 @@ import org.springframework.stereotype.Service;
 public class HomeService {
 
 	private final HomeNodesService nodesService;
+	private final HomeCatsService catsService;
 
 	public HomeNodesService nodes() {
 		return this.nodesService;
+	}
+
+	public HomeCatsService cats() {
+		return this.catsService;
 	}
 
 }
