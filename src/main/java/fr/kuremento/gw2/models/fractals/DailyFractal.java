cef897c32284ae870ed4aa8c63e22be07fc29984
Kurement0 <clem.leborgne@gmail.com>
2024-12-10 10:52:43 +0100

    Serializable #minor

--- src/main/java/fr/kuremento/gw2/models/fractals/DailyFractal.java
+++ src/main/java/fr/kuremento/gw2/models/fractals/DailyFractal.java
@@ -1,6 +1,7 @@
 package fr.kuremento.gw2.models.fractals;
 
+import java.io.Serializable;
 import java.util.List;
 
-public record DailyFractal(int level, String name, List<String> instabilities) {
+public record DailyFractal(int level, String name, List<String> instabilities) implements Serializable {
 }
