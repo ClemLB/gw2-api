d7981e4b9e77438af30cbd8ea5a68a063cb6d24b
Kurement0 <clem.leborgne@gmail.com>
2026-02-25 15:01:46 +0100

    Fix TU #minor

--- src/test/java/fr/kuremento/gw2/web/rest/services/account/home/AccountHomeTest.java
+++ src/test/java/fr/kuremento/gw2/web/rest/services/account/home/AccountHomeTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -26,12 +25,4 @@ class AccountHomeTest {
 		assertNotNull(service.cats(), "Service should not null");
 	}
 
-	@Test
-	@DisplayName("Check get endpoints")
-	void test3() {
-		var endpoints = service.get();
-		assertThat(endpoints).hasSize(2);
-		assertThat(endpoints.get(0)).as("Le premier endpoint devrait être 'nodes'").isEqualTo("nodes");
-		assertThat(endpoints.get(1)).as("Le deuxième endpoint devrait être 'cats'").isEqualTo("cats");
-	}
 }
